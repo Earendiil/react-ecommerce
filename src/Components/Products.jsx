330422314e6c3e2082204d3fe5f9be8da54ae850
teo <teo.th@hotmail.gr>
2025-03-26 22:10:39 +0100

    post 361 Setting Filter Interface

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -2,7 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "./ProductCard";
 import { BiError } from "react-icons/bi";
 import { useEffect } from "react";
-import { fetchProducts } from "../store/avtions";
+import { fetchProducts } from "../store/actions";
+import Filter from "./Filter.jsx";
 
     const Products = () => {
 
@@ -23,6 +24,9 @@ import { fetchProducts } from "../store/avtions";
 
         return (
             <div className="flex flex-wrap lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
+                <div className="w-full mb-6">
+                   <Filter />
+                </div>
                 {isLoading ? (
                     <p>It is loading...</p>
                 ) : errorMessage ? (
